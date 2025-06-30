5bbcc7a26644efced03c50426ccd20c8f6bef2e7
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-01 01:45:58 +0900

    #5 chore: 보안 설정 추가

--- src/main/java/org/sopt/solply_server/global/config/SwaggerConfig.java
+++ src/main/java/org/sopt/solply_server/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.sopt.solply_server.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,23 @@ public class SwaggerConfig {
                 .description("SOPT 앱잼 - SOLPLY API입니다")
                 .version("1.0");
 
+        // 모든 엔드포인트에 JWT 인증 요구사항 적용 (추후 다시 고려. 필요에 따라 개별 API에 @SecurityRequirement 적용 가능)
+        String jwtSchemeName = "JWTToken";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // JWT 인증을 위한 SecurityScheme 추가
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
                 .info(info)
-                .components(new Components());
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes(jwtSchemeName, bearerAuth));
 
     }
 
