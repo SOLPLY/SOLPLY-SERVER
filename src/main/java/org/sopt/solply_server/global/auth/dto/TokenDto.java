c2e153e293d656f985b0be63972efddbb6072706
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-03 22:57:06 +0900

    #27 feat: 토큰 DTO 생성

---
+++ src/main/java/org/sopt/solply_server/global/auth/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package org.sopt.solply_server.global.auth.dto;
+
+import lombok.Builder;
+
+@Builder
+public record TokenDto(
+        String accessToken,
+        String refreshToken
+) {
+    public static TokenDto of(String accessToken, String refreshToken) {
+        return new TokenDto(accessToken, refreshToken);
+    }
+}
