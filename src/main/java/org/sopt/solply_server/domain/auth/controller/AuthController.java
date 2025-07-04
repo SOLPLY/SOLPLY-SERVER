a00ce7d9e9301ac2b9dbf249059bf4c285cc756b
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-05 07:01:41 +0900

    #27 fix: 응답 형식 맞추기

--- src/main/java/org/sopt/solply_server/domain/auth/controller/AuthController.java
+++ src/main/java/org/sopt/solply_server/domain/auth/controller/AuthController.java
@@ -2,7 +2,9 @@ package org.sopt.solply_server.domain.auth.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.solply_server.domain.auth.service.AuthService;
+import org.sopt.solply_server.global.dto.CustomApiResponse;
 import org.sopt.solply_server.global.dto.TokenDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +17,18 @@ public class AuthController {
 
     // 카카오 로그인 성공 후 -> 우리 서버의 토큰 발급
     @PostMapping("/login/kakao")
-    public ResponseEntity<TokenDto> kakaoLogin(@RequestParam String kakaoAccessToken) {
+    public ResponseEntity<CustomApiResponse<TokenDto>> kakaoLogin(@RequestParam String kakaoAccessToken) {
         // kakaoAccessToken으로 카카오 서버에서 사용자 정보 받아오기
         // 받아온 정보로 우리 서비스에 회원가입 또는 로그인 처리 -> memberId 반환
 
         Long memberId = 1L; // 임시 memberId
         TokenDto tokenDto = authService.socialLogin(memberId);
-        return ResponseEntity.ok(tokenDto);
+        return CustomApiResponse.success(HttpStatus.OK, "로그인에 성공했습니다.", tokenDto);
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<TokenDto> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
+    public ResponseEntity<CustomApiResponse<TokenDto>> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
         TokenDto tokenDto = authService.reissueToken(refreshToken);
-        return ResponseEntity.ok(tokenDto);
+        return CustomApiResponse.success(HttpStatus.OK, "토큰 재발급에 성공했습니다.", tokenDto);
     }
 }
