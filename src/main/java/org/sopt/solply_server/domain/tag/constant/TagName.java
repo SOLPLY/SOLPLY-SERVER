e4677ac7d0b31c847d19df85857bb9c638f13869
Minkyu Shin <digi1k2001@gmail.com>
2025-07-09 16:14:02 +0900

    #40 feat: Tag 엔티티 생성

---
+++ src/main/java/org/sopt/solply_server/domain/tag/constant/TagName.java
@@ -0,0 +1,4 @@
+package org.sopt.solply_server.domain.tag.constant;
+
+public enum TagName {
+}
---
+++ src/main/java/org/sopt/solply_server/domain/tag/entity/Tag.java
@@ -0,0 +1,39 @@
+package org.sopt.solply_server.domain.tag.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.solply_server.domain.tag.constant.TagName;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TagName name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Tag parent;
+
+}
