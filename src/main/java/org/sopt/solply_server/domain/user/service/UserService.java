5d3ba5e97aef4a4eedf62b9cc2cfc72eb26e938f
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-09 22:35:10 +0900

    #39 chore: 불필요 import 제거

--- src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
+++ src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
@@ -1,11 +1,7 @@
 package org.sopt.solply_server.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.sopt.solply_server.domain.auth.constant.SocialPlatform;
 import org.sopt.solply_server.domain.user.dto.response.NicknameCheckResponse;
-import org.sopt.solply_server.domain.user.entity.SocialUserInfo;
-import org.sopt.solply_server.domain.user.entity.User;
-import org.sopt.solply_server.domain.user.repository.SocialUserInfoRepository;
 import org.sopt.solply_server.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
