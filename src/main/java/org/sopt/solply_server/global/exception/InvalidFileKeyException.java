9620bfe5df924114dffa2cb5e90e3b5fdddd042b
Minkyu Shin <digi1k2001@gmail.com>
2025-07-10 04:07:04 +0900

    #48 chore: InvalidFileKeyException 제거

--- src/main/java/org/sopt/solply_server/global/exception/InvalidFileKeyException.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.solply_server.global.exception;
-
-public class InvalidFileKeyException extends BusinessException {
-    public InvalidFileKeyException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
--- src/main/java/org/sopt/solply_server/global/util/s3/ImageUrlProvider.java
+++ src/main/java/org/sopt/solply_server/global/util/s3/ImageUrlProvider.java
@@ -1,9 +1,7 @@
 package org.sopt.solply_server.global.util.s3;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.sopt.solply_server.global.exception.InvalidFileKeyException;
 import org.sopt.solply_server.global.util.InputValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,7 +15,6 @@ public class ImageUrlProvider {
     private String cloudFrontDomain;
 
     public String getImageUrl(String fileKey) {
-        log.info(String.format("https://%s/%s", cloudFrontDomain, fileKey));
         if (InputValidator.isNull(fileKey)) return null;
         return String.format("https://%s/%s", cloudFrontDomain, fileKey);
     }
