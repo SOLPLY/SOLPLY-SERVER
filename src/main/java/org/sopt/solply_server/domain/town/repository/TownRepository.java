4a592eb3c1f46178b0f8249c6747b70a4a3eb732
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-10 04:32:46 +0900

    #47 feat: town 엔티티 연관

---
+++ src/main/java/org/sopt/solply_server/domain/town/repository/TownRepository.java
@@ -0,0 +1,8 @@
+package org.sopt.solply_server.domain.town.repository;
+
+import org.sopt.solply_server.domain.town.entity.Town;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TownRepository extends JpaRepository<Town, Long> {
+    boolean existsByName(String name);
+}
--- src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
+++ src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package org.sopt.solply_server.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.solply_server.domain.town.repository.TownRepository;
 import org.sopt.solply_server.domain.user.dto.request.UserUpdateRequest;
 import org.sopt.solply_server.domain.user.dto.response.NicknameCheckResponse;
 import org.sopt.solply_server.domain.user.dto.response.UserUpdateResponse;
@@ -12,12 +14,14 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TownRepository townRepository;
 
     public NicknameCheckResponse checkNickname(String nickname) {
         boolean isDuplicated = userRepository.existsByNickname(nickname);
@@ -30,7 +34,19 @@ public class UserService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_USER));
 
+        if (!user.isNewUser()) {
+            log.warn("이미 온보딩이 완료된 사용자: userId={}", userId);
+            throw new BusinessException(ErrorCode.ONBOARDING_ALREADY_COMPLETED);
+        }
+
+        // 3. 동네 존재 여부 검증
+        if (!townRepository.existsByName(request.favoriteTowns())) {
+            throw new BusinessException(ErrorCode.TOWN_NOT_FOUND);
+        }
+
+        // 4. 닉네임 중복검사
         if (userRepository.existsByNickname(request.nickname())) {
+            log.warn("중복된 닉네임 사용 시도: nickname={}", request.nickname());
             throw new BusinessException(ErrorCode.DUPLICATE_NICKNAME);
         }
 
