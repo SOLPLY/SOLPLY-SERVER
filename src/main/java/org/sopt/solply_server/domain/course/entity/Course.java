c67f40ffb8ecca9282fd87eafc22114011761773
Minkyu Shin <digi1k2001@gmail.com>
2025-07-09 16:33:50 +0900

    #40 feat: Course 엔티티 작성

--- src/main/java/org/sopt/solply_server/domain/course/entity/Course.java
+++ src/main/java/org/sopt/solply_server/domain/course/entity/Course.java
@@ -1,6 +1,10 @@
 package org.sopt.solply_server.domain.course.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -19,13 +23,13 @@ import lombok.NoArgsConstructor;
 )
 public class Course {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String introduction;
-
-
-
 
 }
