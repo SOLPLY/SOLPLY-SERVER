f05bd7fb6139055f66c9166d74d568e9f1fa3d9b
Minkyu Shin <digi1k2001@gmail.com>
2025-07-10 03:53:03 +0900

    #48 chore: sql 더미데이터 스크립트 추가

---
+++ src/main/java/org/sopt/solply_server/global/exception/InvalidFileKeyException.java
@@ -0,0 +1,7 @@
+package org.sopt.solply_server.global.exception;
+
+public class InvalidFileKeyException extends RuntimeException {
+  public InvalidFileKeyException(String message) {
+    super(message);
+  }
+}
