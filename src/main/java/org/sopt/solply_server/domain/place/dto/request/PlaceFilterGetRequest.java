7f0efd69179cdd600981b8614e3d4d8711ea7dff
Minkyu Shin <digi1k2001@gmail.com>
2025-07-10 23:18:42 +0900

    #48 fix: dto 에러 메시지 작성

--- src/main/java/org/sopt/solply_server/domain/place/dto/request/PlaceFilterGetRequest.java
+++ src/main/java/org/sopt/solply_server/domain/place/dto/request/PlaceFilterGetRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record PlaceFilterGetRequest (
-        @NotNull Long townId,
+        @NotNull(message = "동네 아이디 값은 필수입니다.") Long townId,
         Long mainTagId,
         List<Long> subTagAIdList,
         List<Long> subTagBIdList
