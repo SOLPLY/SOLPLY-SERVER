bf9c0dac2124e4e5804c669d2c65e3574c9751a4
Minkyu Shin <digi1k2001@gmail.com>
2025-07-11 03:43:02 +0900

    #57 fix: docker-compose.yml 배포 파이프라인 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+COMPOSE_CONTENT="$1"
 
-echo "🚀 배포 시작"
+echo "🚀 배포 시작: $IMAGE_NAME"
 
 cd /home/ubuntu/solply-server
 
-echo "✅ docker-compose.yml 내용 확인"
-cat docker-compose.yml  # 디버깅용
+echo "$COMPOSE_CONTENT" > docker-compose.yml
+echo "✅ docker-compose.yml 생성 완료"
 
 # 배포 실행
 docker-compose pull app || true
