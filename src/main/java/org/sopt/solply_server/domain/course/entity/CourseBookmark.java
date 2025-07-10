e4ff4ca2a509cc6496cf3583850857d7f6cf6c86
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-11 06:12:04 +0900

    #63 feat: course 공유여부 필드 추가 및 CourseBookmark 엔티티 생성

---
+++ src/main/java/org/sopt/solply_server/domain/course/entity/CourseBookmark.java
@@ -0,0 +1,45 @@
+package org.sopt.solply_server.domain.course.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.solply_server.domain.user.entity.User;
+import org.sopt.solply_server.global.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "course_bookmark",
+        indexes = {
+                @Index(name = "idx_course_bookmark_user_course",
+                        columnList = "user_id, course_id",
+                        unique = true
+                )
+        }
+)
+public class CourseBookmark extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
