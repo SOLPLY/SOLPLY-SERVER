7f4aa70fc69d486829d644103c2c92f11423df46
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-12 02:43:28 +0900

    #63 chore: 빌드 오류 해결

--- src/main/java/org/sopt/solply_server/domain/place/repository/PlaceBookmarkRepository.java
+++ src/main/java/org/sopt/solply_server/domain/place/repository/PlaceBookmarkRepository.java
@@ -1,14 +1,11 @@
 package org.sopt.solply_server.domain.place.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import org.sopt.solply_server.domain.place.entity.PlaceBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Set;
 
 public interface PlaceBookmarkRepository extends JpaRepository<PlaceBookmark, Long> {
@@ -21,8 +18,4 @@ public interface PlaceBookmarkRepository extends JpaRepository<PlaceBookmark, Lo
     boolean existsByUserIdAndPlaceId(Long userId, Long placeId);
 
     void deleteByUserIdAndPlaceId(Long userId, @Param("placeId") Long placeId);
-
-    @Modifying(clearAutomatically = true)
-    @Query("DELETE FROM PlaceBookmark pb WHERE pb.user.id = :userId AND pb.place.id IN :placeIds")
-    void deleteByUserIdAndPlaceIdInPlaceIdList(@Param("userId") Long userId, @Param("placeIds") List<Long> placeIdList);
 }
