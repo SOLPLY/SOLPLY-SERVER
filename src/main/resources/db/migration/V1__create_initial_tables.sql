16dc168bbf1df350b61f86f491646bc1431daafb
Minkyu Shin <digi1k2001@gmail.com>
2025-07-15 01:35:11 +0900

    #53 chore: flyway gradle 세팅

--- src/main/resources/db/migration/V1__create_initial_tables.sql
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -211,10 +211,10 @@ ALTER TABLE social_user_info ADD CONSTRAINT uk_social_user_info_social_code UNIQ
 
 -- updated_at 자동 업데이트 함수
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
-RETURN NEW;
+    RETURN NEW;
 END;
 $$ language 'plpgsql';
 
