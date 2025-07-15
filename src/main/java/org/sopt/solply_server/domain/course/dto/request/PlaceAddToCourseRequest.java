ae7566c366f0c595859ba33dc3c3e48edee17dcd
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-15 23:37:24 +0900

    #96 chore: 태그 이름 변경

--- src/main/java/org/sopt/solply_server/domain/course/dto/request/PlaceAddToCourseRequest.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.solply_server.domain.course.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record PlaceAddToCourseRequest(
-        @NotNull(message = "placeId는 필수입니다")
-        Long placeId
-) {
-}
---
+++ src/main/java/org/sopt/solply_server/domain/course/dto/response/PlaceAddToCoursesResponse.java
@@ -0,0 +1,4 @@
+package org.sopt.solply_server.domain.course.dto.response;
+
+public record PlaceAddToCoursesResponse() {
+}
