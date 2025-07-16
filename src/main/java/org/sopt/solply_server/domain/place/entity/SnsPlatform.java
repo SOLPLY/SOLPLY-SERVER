420213df03121026aceb382dbab461fe9c4a63ed
bykbyk0401 <bykbyk0401@gmail.com>
2025-07-17 03:15:57 +0900

    #120 fix: 플랫폼 한글 반환

--- src/main/java/org/sopt/solply_server/domain/place/entity/SnsPlatform.java
+++ src/main/java/org/sopt/solply_server/domain/place/entity/SnsPlatform.java
@@ -1,9 +1,12 @@
 package org.sopt.solply_server.domain.place.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum SnsPlatform {
-    INSTAGRAM;
+    INSTAGRAM("인스타그램");
 
+    private final String displayName;
 }
