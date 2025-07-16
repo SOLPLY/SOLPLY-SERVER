49fd2843ec709ec083ece4ab96a7b548aa60be8c
Minkyu Shin <digi1k2001@gmail.com>
2025-07-16 22:34:13 +0900

    #113 fix: 닉네임 체크 수정

--- src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
+++ src/main/java/org/sopt/solply_server/domain/user/service/UserService.java
@@ -32,14 +32,14 @@ public class UserService {
 
     public NicknameCheckResponse checkNickname(Long userId, String nickname) {
         User user = entityLoader.getUser(userId);
-        boolean isDuplicated;
+        boolean isDuplicated = false;
         try {
             userValidator.validateNickname(user.getNickname(), nickname);
         } catch (BusinessException e) {
             isDuplicated = true;
         }
 
-        return NicknameCheckResponse.of(false);
+        return NicknameCheckResponse.of(isDuplicated);
     }
 
     public UserProfileGetResponse getUserProfile(Long userId) {
