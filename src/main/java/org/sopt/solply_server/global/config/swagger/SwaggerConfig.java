ba239b60f63267690ca12f0952c761d5f6c433ea
Minkyu Shin <digi1k2001@gmail.com>
2026-01-22 19:27:55 +0900

    docs: 스웨거 어드민 페이지랑 서비스 기능 페이지 분리

--- src/main/java/org/sopt/solply_server/global/config/swagger/SwaggerConfig.java
+++ src/main/java/org/sopt/solply_server/global/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.sopt.solply_server.global.config;
+package org.sopt.solply_server.global.config.swagger;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
---
+++ src/main/java/org/sopt/solply_server/global/config/swagger/SwaggerGroupConfig.java
@@ -0,0 +1,26 @@
+package org.sopt.solply_server.global.config.swagger;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerGroupConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .pathsToExclude("/api/admin/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("admin")
+                .pathsToMatch("/api/admin/**")
+                .build();
+    }
+}
