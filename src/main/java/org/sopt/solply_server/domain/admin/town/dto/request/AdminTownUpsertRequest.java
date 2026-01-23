4407f020244a78dbf81f6c93a07887b6aab5015e
ImHyungsuk <limhs8630@gmail.com>
2026-01-23 21:45:38 +0900

    fix: 지역 등록시 townId null 허용

--- src/main/java/org/sopt/solply_server/domain/admin/town/dto/request/AdminTownUpsertRequest.java
+++ src/main/java/org/sopt/solply_server/domain/admin/town/dto/request/AdminTownUpsertRequest.java
@@ -1,7 +1,6 @@
 package org.sopt.solply_server.domain.admin.town.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record AdminTownUpsertRequest(
@@ -9,6 +8,5 @@ public record AdminTownUpsertRequest(
 	@Size(max = 100, message = "동네명은 100자를 초과할 수 없습니다.")
 	String name,
 
-	@NotNull(message = "townId는 필수입니다.")
 	Long townId
 ) {}
