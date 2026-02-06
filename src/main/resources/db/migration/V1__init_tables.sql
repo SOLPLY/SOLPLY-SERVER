077b37ae915240ccac6d25bc7c8539d70c2ff0f0
Minkyu Shin <digi1k2001@gmail.com>
2026-02-06 14:30:39 +0900

    #295 fix: 오류 수정

--- src/main/resources/db/migration/V1__init_tables.sql
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -90,7 +90,7 @@ CREATE TABLE tags (
                       type VARCHAR(50) NOT NULL,
                       parent_id BIGINT NULL,
                       active BOOLEAN NOT NULL DEFAULT TRUE,
-                      usage VARCHAR(20) NOT NULL DEFAULT 'PLACE',t a
+                      usage VARCHAR(20) NOT NULL DEFAULT 'PLACE',
 
                       CONSTRAINT fk_tags_parent
                           FOREIGN KEY (parent_id) REFERENCES tags(id)
