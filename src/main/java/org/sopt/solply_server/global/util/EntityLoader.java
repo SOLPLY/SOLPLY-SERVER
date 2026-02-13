92a53c80d03db9283761b8844087a9a087a744e3
ImHyungsuk <limhs8630@gmail.com>
2026-02-13 19:15:25 +0900

    refactor: 데드 코드 제거

--- src/main/java/org/sopt/solply_server/global/util/EntityLoader.java
+++ src/main/java/org/sopt/solply_server/global/util/EntityLoader.java
@@ -72,11 +72,6 @@ public class EntityLoader {
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUND_TAG));
     }
 
-    public Tag getCourseTag(Long id){
-        return tagRepository.findById(id)
-            .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUND_TAG));
-    }
-
     public PlaceRequest getPlaceRequest(Long requestId) {
         return placeRequestRepository.findById(requestId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUND_PLACE_REQUEST));
