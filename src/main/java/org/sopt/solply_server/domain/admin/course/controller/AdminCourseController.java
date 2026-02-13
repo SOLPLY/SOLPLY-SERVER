3c854e15faf510277784bcdc96c06e13f7c69f4f
ImHyungsuk <limhs8630@gmail.com>
2026-02-13 19:19:37 +0900

    refactor: pathvariable 추가

--- src/main/java/org/sopt/solply_server/domain/admin/course/controller/AdminCourseController.java
+++ src/main/java/org/sopt/solply_server/domain/admin/course/controller/AdminCourseController.java
@@ -13,6 +13,7 @@ import org.sopt.solply_server.global.dto.CustomApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,9 +56,9 @@ public class AdminCourseController {
 	}
 
 	@Operation(summary = "어드민 코스 상세 조회", description = "어드민이 코스 상세 정보를 조회합니다.")
-	@GetMapping
+	@GetMapping("/{id}")
 	public ResponseEntity<CustomApiResponse<AdminCourseDetailResponse>> getCourse(
-		@RequestParam(name = "id") Long courseId
+		@PathVariable("id") Long courseId
 	) {
 		return CustomApiResponse.success(
 			"어드민 코스 상세 조회 성공",
@@ -66,9 +67,9 @@ public class AdminCourseController {
 	}
 
 	@Operation(summary = "어드민 코스 수정", description = "어드민이 코스를 수정합니다.")
-	@PatchMapping()
+	@PatchMapping("/{id}")
 	public ResponseEntity<CustomApiResponse<AdminCourseUpdateResponse>> updateCourse(
-		@RequestParam Long courseId,
+		@PathVariable("id") Long courseId,
 		@Valid @RequestBody AdminCourseUpdateRequest request
 	) {
 		return CustomApiResponse.success(
@@ -80,7 +81,7 @@ public class AdminCourseController {
 	@Operation(summary = "어드민 코스 상태 수정", description = "어드민이 코스의 활성화 상태를 수정합니다.")
 	@PatchMapping("/{id}/activation")
 	public ResponseEntity<CustomApiResponse<AdminCourseUpsertResponse>> updateCourseStatus(
-		@RequestParam("id") Long courseId,
+		@PathVariable("id") Long courseId,
 		@Valid @RequestBody AdminCourseActivationRequest request
 	) {
 		return CustomApiResponse.success(
