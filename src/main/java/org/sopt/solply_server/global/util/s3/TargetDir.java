10d421bfe837e54bbdbe4e7346c550b326dbfc75
88guri <quffl2002@naver.com>
2026-03-11 12:19:04 +0900

    #333 feat: s3에 RECORD 업로드 디렉토리 추가

--- src/main/java/org/sopt/solply_server/global/util/s3/TargetDir.java
+++ src/main/java/org/sopt/solply_server/global/util/s3/TargetDir.java
@@ -9,7 +9,8 @@ public enum TargetDir {
     PLACE("places"),
     PLACE_REQUEST("place-requests"),
     PLACE_REPORT("place-reports"),
-    USER_PROFILE("user-profiles");
+    USER_PROFILE("user-profiles"),
+    RECORD("records");
 
     private final String dir;
 
