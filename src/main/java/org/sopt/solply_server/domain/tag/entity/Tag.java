367cbe5e5c193e9f40c8ab4fa074209d0e168b3c
Minkyu Shin <digi1k2001@gmail.com>
2026-03-13 00:54:55 +0900

    #339 feat: tag별 부연 설명 추가 및 retrieval_text 생성기 구현

--- src/main/java/org/sopt/solply_server/domain/tag/entity/Tag.java
+++ src/main/java/org/sopt/solply_server/domain/tag/entity/Tag.java
@@ -47,6 +47,9 @@ public class Tag {
 
     private String name;
 
+    @Column(columnDefinition = "TEXT")
+    private String sentence;
+
     @Enumerated(EnumType.STRING)
     private TagType type;
 
