26228815adb90b1d6fe1f57834c2f9fd2e53d03e
Minkyu Shin <digi1k2001@gmail.com>
2026-03-14 00:01:18 +0900

    fix: SwaggerConfig 프로파일을 dev에서 !prod로 변경하여 로컬 환경에서도 인증 설정 활성화

--- src/main/java/org/sopt/solply_server/global/config/swagger/SwaggerConfig.java
+++ src/main/java/org/sopt/solply_server/global/config/swagger/SwaggerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev")
+@Profile("!prod")
 @Configuration
 public class SwaggerConfig {
 
