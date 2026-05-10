649fc78a27cf02be4bb54be85f4b56cd37c7bd78
88guri <quffl2002@naver.com>
2026-05-10 23:06:21 +0900

    #372 refact: 회원 정보 조회 API에 내 리뷰 미리보기 추가

--- src/main/java/org/sopt/solply_server/domain/user/dto/response/UserProfileGetResponse.java
+++ src/main/java/org/sopt/solply_server/domain/user/dto/response/UserProfileGetResponse.java
@@ -1,28 +1,36 @@
 package org.sopt.solply_server.domain.user.dto.response;
 
 import java.util.List;
+import org.sopt.solply_server.domain.review.dto.response.GetMyReviewPreviewResponse;
 import org.sopt.solply_server.domain.user.dto.UserPlacePreviewDto;
 import org.sopt.solply_server.domain.user.dto.UserTownInfoDto;
 import org.sopt.solply_server.domain.user.entity.User;
 import org.sopt.solply_server.domain.user.entity.UserPersona;
 
 public record UserProfileGetResponse(
-        Long userId,
-        String nickname,
-        String profileImageUrl,
-        UserTownInfoDto selectedTown,
-        UserPersona persona,
-        List<UserPlacePreviewDto> myPlacePreviews
+    Long userId,
+    String nickname,
+    String profileImageUrl,
+    UserTownInfoDto selectedTown,
+    UserPersona persona,
+    List<UserPlacePreviewDto> myPlacePreviews,
+    GetMyReviewPreviewResponse myReviewPreview
 ) {
-    public static UserProfileGetResponse of(User user, String profileImageUrl, UserTownInfoDto selectedTown,
-            List<UserPlacePreviewDto> myPlacePreviews) {
-        return new UserProfileGetResponse(
-                user.getId(),
-                user.getNickname(),
-                profileImageUrl,
-                selectedTown,
-                user.getPersona(),
-                myPlacePreviews
-        );
-    }
+  public static UserProfileGetResponse of(
+      User user,
+      String profileImageUrl,
+      UserTownInfoDto selectedTown,
+      List<UserPlacePreviewDto> myPlacePreviews,
+      GetMyReviewPreviewResponse myReviewPreview
+  ) {
+    return new UserProfileGetResponse(
+        user.getId(),
+        user.getNickname(),
+        profileImageUrl,
+        selectedTown,
+        user.getPersona(),
+        myPlacePreviews,
+        myReviewPreview
+    );
+  }
 }
