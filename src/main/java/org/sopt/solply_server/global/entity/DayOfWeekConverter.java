d75ad9c262a828333607077696e0598be87a3742
ImHyungsuk <limhs8630@gmail.com>
2026-06-08 19:14:30 +0900

    feat: 요일컨버터 null처리

--- src/main/java/org/sopt/solply_server/global/entity/DayOfWeekConverter.java
+++ src/main/java/org/sopt/solply_server/global/entity/DayOfWeekConverter.java
@@ -4,14 +4,20 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class DayOfWeekConverter implements AttributeConverter<DayOfWeek,Integer> {
+public class DayOfWeekConverter implements AttributeConverter<DayOfWeek, Integer> {
 	@Override
 	public Integer convertToDatabaseColumn(DayOfWeek dayOfWeek) {
+		if (dayOfWeek == null) {
+			return null;
+		}
 		return dayOfWeek.getValue();
 	}
 
 	@Override
 	public DayOfWeek convertToEntityAttribute(Integer integer) {
+		if (integer == null) {
+			return null;
+		}
 		return DayOfWeek.from(integer);
 	}
 }
